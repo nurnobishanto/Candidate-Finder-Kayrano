f500f87aae7d78698fb58a683fab939fbd76b1b2
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-04-05 22:49:30 +0600

    update

--- resources/views/admin/menus/list-for-delete.blade.php
+++
@@ -1,3 +0,0 @@
-@foreach($items as $item)
-    <option value="{{$item['menu_id']}}">{{__($item['title'])}} ({{$item['menu_id']}})</option>
-@endforeach
--- resources/views/admin/roles/permissions-dd.blade.php
+++
@@ -1,9 +0,0 @@
-<select class="duallistbox" multiple="multiple" id="permissions-multiselect">
-    @foreach($permissions as $category => $permission)
-    <optgroup label="{{$category}}">
-        @foreach($permission as $perm)
-        <option value="{{$perm['id']}}" {{$perm['selected'] == 1 ? 'selected="selected"' : ''}}>{{$perm['title']}}</option>
-        @endforeach                                    
-    </optgroup>
-    @endforeach
-</select>
